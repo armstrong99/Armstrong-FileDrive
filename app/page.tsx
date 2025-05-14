63df1e3c6ee461cd9eb67775f8521a38918d2d49
Ndukwe Armstrong <ndukwearmstrong@Mac.lan>
2025-05-14 05:19:42 +0100

    resolving nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { nextAuthOptions } from "./api/auth/[...nextauth]/route";
+import { nextAuthOptions } from "./lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(nextAuthOptions);
